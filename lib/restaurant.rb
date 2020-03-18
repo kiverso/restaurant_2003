85df5f6741543076ffb3ddfde2a38e225c7226cc
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-03-18 13:23:55 -0600

    Add iteration 1 functionality.

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,10 @@
+class Restaurant
+  attr_reader :name, :opening_time, :dishes
 
+  def initialize(opening_time, name, dishes = [])
+    @opening_time = opening_time
+    @name = name
+    @dishes = dishes
+  end
+
+end
